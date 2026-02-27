77bc0ef84e6f8e90c5d8724735cadfae4b8b44ce
andresprovira <113343582+andresprovira@users.noreply.github.com>
2026-02-27 12:40:20 +0100

    Create original SQL script file for functions and SELECT queries

---
+++ v1-original/funcion-tabla-fusion-original.sql
@@ -0,0 +1,27 @@
+-- SQL Script containing original functions and queries
+
+-- Function: fusion_cable_tp
+CREATE FUNCTION fusion_cable_tp(...) RETURNS ... AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function: fusion_tp_tp
+CREATE FUNCTION fusion_tp_tp(...) RETURNS ... AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function: recorridoCable
+CREATE FUNCTION recorridoCable(...) RETURNS ... AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+-- SELECT Queries
+SELECT * FROM table_name WHERE condition;
+
+-- Additional SQL queries or comments can go here...
